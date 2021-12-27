7ac288c2a37924a28b6c0cf07e9acf190d14ff0d
Roosterseatyou <ltbkthree@icloud.com>
2021-12-27 13:37:39 -0500

    And so it begins...

---
+++ src/main/java/xyz/roosterseatyou/mobitems/MobItems.java
@@ -0,0 +1,17 @@
+package xyz.roosterseatyou.mobitems;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MobItems extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
