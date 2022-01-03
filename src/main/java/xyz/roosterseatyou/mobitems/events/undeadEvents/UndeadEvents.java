0fb1a7c363a0bb2465cff9e4c605c38bad2acb9e
YoussefO07 <youssef.moh.omar@gmail.com>
2022-01-03 21:27:48 +0200

    Finished Undead Events (added onDeath).

--- src/main/java/xyz/roosterseatyou/mobitems/events/undeadEvents/UndeadEvents.java
+++ src/main/java/xyz/roosterseatyou/mobitems/events/undeadEvents/UndeadEvents.java
@@ -1,12 +1,19 @@
 package xyz.roosterseatyou.mobitems.events.undeadEvents;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.potion.PotionEffect;
 import org.bukkit.scheduler.BukkitScheduler;
+import org.bukkit.event.Listener;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.event.entity.EntityDamageEvent;
 import xyz.roosterseatyou.mobitems.MobItems;
 import xyz.roosterseatyou.mobitems.utils.mobarmorutils.UndeadArmorUtils;
 
 
-public class UndeadEvents{
+public class UndeadEvents implements Listener{
 
     public static void playerBurn(){
 
@@ -22,6 +29,20 @@ public class UndeadEvents{
 
             }
         }, 0L, 100L);
+    }
+    @EventHandler
+    public static void onDeath(EntityDamageEvent e){
+        Entity ent= e.getEntity();
+        if (ent instanceof Player){
+            Player p = (Player)ent;
+
+            if (e.getDamage() > p.getHealth()){
+                p.setHealth(p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getDefaultValue());
+                p.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION , 125, 2));
+                p.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE , 125, 1));
+            }
+        }
+
     }
 
 }
