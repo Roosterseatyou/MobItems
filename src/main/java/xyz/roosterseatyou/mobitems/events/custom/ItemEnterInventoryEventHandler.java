97b369b49920dcaae035fa6179e8ce791057ea1c
Roosterseatyou <ltbkthree@icloud.com>
2022-02-16 12:24:24 -0600

    Not able to test anything atm, but it *should* work, which means it probably won't.

--- src/main/java/xyz/roosterseatyou/mobitems/events/custom/ItemEnterInventoryEventHandler.java
+++ src/main/java/xyz/roosterseatyou/mobitems/events/custom/ItemEnterInventoryEventHandler.java
@@ -1,13 +1,27 @@
 package xyz.roosterseatyou.mobitems.events.custom;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class ItemEnterInventoryEventHandler implements Listener {
     @EventHandler
     public void onPickup(EntityPickupItemEvent e){
-        Bukkit.getServer().getPluginManager().callEvent(new ItemEnterInventoryEvent(e.g));
+        if(e.getEntity() instanceof Player) {
+            Player p = (Player) e.getEntity();
+            Bukkit.getServer().getPluginManager().callEvent(new PlayerObtainItemEvent(p, e.getItem().getItemStack(), PlayerObtainItemEvent.Reason.PICK_UP));
+        }
+    }
+
+    @EventHandler
+    public void moveItem(InventoryClickEvent e){
+        Player p = (Player) e.getWhoClicked();
+        if(e.getAction() == InventoryAction.MOVE_TO_OTHER_INVENTORY && e.getClickedInventory() == p.getInventory()){
+            Bukkit.getServer().getPluginManager().callEvent(new PlayerObtainItemEvent(p, e.getCurrentItem(), PlayerObtainItemEvent.Reason.PICK_UP));
+        }
     }
 }
--- src/main/java/xyz/roosterseatyou/mobitems/events/custom/PlayerObtainItemEvent.java
+++ src/main/java/xyz/roosterseatyou/mobitems/events/custom/PlayerObtainItemEvent.java
@@ -1,21 +1,35 @@
 package xyz.roosterseatyou.mobitems.events.custom;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.inventory.InventoryEvent;
+import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-public class ItemEnterInventoryEvent extends InventoryEvent {
+import javax.net.ssl.HandshakeCompletedEvent;
+
+public class PlayerObtainItemEvent extends PlayerEvent {
     private ItemStack item;
     private Reason reason;
+    private static final HandlerList handlers = new HandlerList();
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return null;
+    }
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
 
     enum Reason{
         PICK_UP,
         FROM_INVENTORY
     }
 
-    public ItemEnterInventoryEvent(@NotNull InventoryView transaction, ItemStack item, Reason reason) {
-        super(transaction);
+    public PlayerObtainItemEvent(@NotNull Player player, ItemStack item, Reason reason) {
+        super(player);
         this.item = item;
         this.reason = reason;
     }
