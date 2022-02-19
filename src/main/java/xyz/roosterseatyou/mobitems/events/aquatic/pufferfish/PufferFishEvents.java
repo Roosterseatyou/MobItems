988e5df5067d0a149162520c8b72460e7c6b6731
Roosterseatyou <ltbkthree@icloud.com>
2022-02-19 16:29:32 -0600

    I think its ready for alpha 1.2?

--- src/main/java/xyz/roosterseatyou/mobitems/events/aquatic/pufferfish/PufferFishEvents.java
+++ src/main/java/xyz/roosterseatyou/mobitems/events/aquatic/pufferfish/PufferFishEvents.java
@@ -1,34 +1,44 @@
 package xyz.roosterseatyou.mobitems.events.aquatic.pufferfish;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Mob;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
+import xyz.roosterseatyou.mobitems.MobItems;
 import xyz.roosterseatyou.mobitems.utils.mobarmorutils.AquaticUtils;
 
 public class PufferFishEvents implements Listener {
+
     @EventHandler
     public void onPlayerHit(EntityDamageByEntityEvent e){
         if(e.getEntity() instanceof Player p) {
             int amt = AquaticUtils.getPufferArmorCount(p);
-            if (e.getDamager() instanceof LivingEntity damager) {
+            if (e.getDamager() instanceof Player damager) {
                 if (amt != 0) {
                     if (amt == 2) {
                         damager.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 5*20, 1));
+                        p.sendMessage("amt == 2");
                     } else if (amt == 1) {
                         damager.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 5*20, 0));
+                        p.sendMessage("amt == 1");
                     }
                 }
+
             } else if(e.getDamager() instanceof Arrow arrow){
-                if(arrow.getShooter() instanceof LivingEntity shooter){
+                if(arrow.getShooter() instanceof Player shooter){
                     if (amt == 2) {
                         shooter.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 5*20, 1));
+                        p.sendMessage("amt == 2");
                     } else if (amt == 1) {
                         shooter.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 5*20, 0));
+                        p.sendMessage("amt == 1");
                     }
                 }
             }
