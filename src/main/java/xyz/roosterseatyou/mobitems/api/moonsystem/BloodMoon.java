d05d5c8ad659e0db97baf5395278890b2534b945
Roosterseatyou <ltbkthree@icloud.com>
2022-07-22 14:43:55 -0500

    Changes to BloodMoon

--- src/main/java/xyz/roosterseatyou/mobitems/api/moonsystem/BloodMoon.java
+++ src/main/java/xyz/roosterseatyou/mobitems/api/moonsystem/BloodMoon.java
@@ -1,15 +1,18 @@
 package xyz.roosterseatyou.mobitems.api.moonsystem;
 
+import com.destroystokyo.paper.event.entity.EntityAddToWorldEvent;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.Mob;
-import xyz.roosterseatyou.mobitems.MobItems;
-import xyz.roosterseatyou.mobitems.mobgoals.Hostile;
+import org.bukkit.entity.Monster;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
-public class BloodMoon extends MoonPhase {
+public class BloodMoon extends MoonPhase implements Listener {
     public static BloodMoon BLOOD_MOON = new BloodMoon();
 
     private BloodMoon() {
@@ -18,6 +21,11 @@ public class BloodMoon extends MoonPhase {
         setMoonStartEventHandler((e) -> {
             if(e.getMoonPhase() == BLOOD_MOON) {
                 Bukkit.broadcast(Component.text("You feel an eerie chill as a Blood Moon rises...").color(TextColor.color(64, 4, 4)).decorate(TextDecoration.ITALIC));
+            }
+            for(Entity ent : e.getWorld().getEntities()) {
+                if(ent instanceof Monster mon) {
+                    mon.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 12000, 1));
+                }
             }
         });
         register();
