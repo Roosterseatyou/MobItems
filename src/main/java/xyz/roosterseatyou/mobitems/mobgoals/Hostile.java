b959cacb5802cde531c07f1a20fcbb9e1e942073
Roosterseatyou <ltbkthree@icloud.com>
2022-07-23 01:02:08 -0500

    Fixes to the logic (Hostile Goal).

--- src/main/java/xyz/roosterseatyou/mobitems/mobgoals/Hostile.java
+++ src/main/java/xyz/roosterseatyou/mobitems/mobgoals/Hostile.java
@@ -15,16 +15,16 @@ import xyz.roosterseatyou.mobitems.MobItems;
 import java.util.EnumSet;
 
 public class Hostile implements Goal<Mob> {
-    private final GoalKey<Mob> key;
+    private static final GoalKey<Mob> key = GoalKey.of(Mob.class, new NamespacedKey(MobItems.getInstance(), "hostility"));
     private final Mob entity;
     private final double range;
     private final double speed;
     private final double attackRange;
     private final double attackInterval;
     private int cooldown;
+    private Player target;
 
     public Hostile(@NotNull Mob entity, double range, double speed, double attackRange, double attackInterval) {
-        this.key = GoalKey.of(Mob.class, new NamespacedKey(MobItems.getInstance(), "hostility"));
         this.entity = entity;
         this.range = range;
         this.speed = speed;
@@ -34,23 +34,26 @@ public class Hostile implements Goal<Mob> {
 
     @Override
     public void tick() {
-        locatePlayer();
-        if (entity.getTarget() == null) return;
-        if (entity.getLocation().distance(entity.getTarget().getLocation()) > range) {
+        if (target == null){
+            locatePlayer();
+            return;
+        }
+        if (entity.getLocation().distance(target.getLocation()) > range) {
             entity.setTarget(null);
+            target = null;
             return;
         }
 
-        if (entity.getLocation().distance(entity.getTarget().getLocation()) < range) {
-            entity.getPathfinder().moveTo(entity.getTarget().getLocation(), speed);
+        if (entity.getLocation().distance(target.getLocation()) < range) {
+            entity.getPathfinder().moveTo(target.getLocation(), speed);
         }
-        if (entity.getLocation().distance(entity.getTarget().getLocation()) < attackRange) {
+        if (entity.getLocation().distance(target.getLocation()) < attackRange) {
             if (cooldown <= 0) {
-                entity.getTarget().damage(2);
+                target.damage(2);
                 cooldown = (int) attackInterval;
-                if (entity.getTarget().isDead() || entity.getTarget().getHealth() <= 0) {
+                if (target.isDead() || target.getHealth() <= 0) {
                     entity.setTarget(null);
-                    Bukkit.getServer().broadcast(Component.text("<The Blood God> Blood for the one and only Blood God.").color(TextColor.color(66, 3, 6)));
+                    target = null;
                     entity.getWorld().playSound(entity.getLocation(), "entity.lightning_bolt.thunder", 1000, 1);
                 }
             } else {
@@ -64,16 +67,20 @@ public class Hostile implements Goal<Mob> {
             if(player.getLocation().distance(entity.getLocation()) < range) {
                 if(player.isDead() || player.getHealth() <= 0) continue;
                 entity.setTarget(player);
+                target = player;
+                return;
             }
         }
     }
 
     @Override
     public boolean shouldActivate() {
-        if (entity.getTarget() == null || entity.getTarget().isDead()) {
+        MobItems.log.info("[" + entity.getType().name() + "] Hostile shouldActivate");
+        if (target == null || target.isDead()) {
+            locatePlayer();
             return false;
         }
-        return !(entity.getLocation().distance(entity.getTarget().getLocation()) > range);
+        return !(entity.getLocation().distance(target.getLocation()) > range);
     }
 
     @Override
@@ -89,5 +96,9 @@ public class Hostile implements Goal<Mob> {
     @Override
     public boolean shouldStayActive() {
         return shouldActivate();
+    }
+
+    public static GoalKey<Mob> getStaticKey() {
+        return key;
     }
 }
